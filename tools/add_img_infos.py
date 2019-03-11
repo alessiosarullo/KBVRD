06f09573a1758ad1d34b87413d8e1aaaf27d7451
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-03-11 10:08:00 +0000

    img infos tool

---
+++ tools/add_img_infos.py
@@ -0,0 +1,51 @@
+import sys
+
+import h5py
+import numpy as np
+
+from config import cfg
+from lib.dataset.hicodet import HicoDetInstanceSplit
+from lib.dataset.utils import Splits, Minibatch
+
+
+def save_feats():
+    batch_size = 1
+    flipping = False
+
+    sys.argv += ['--model', 'base', '--save_dir', 'fake']  # fake required arguments
+    sys.argv += ['--batch_size', str(batch_size), '--val_ratio', '0']
+    cfg.parse_args()
+
+    train_split = HicoDetInstanceSplit.get_split(split=Splits.TRAIN, flipping_prob=float(flipping))
+    test_split = HicoDetInstanceSplit.get_split(split=Splits.TEST, flipping_prob=float(flipping))
+    assert Splits.VAL not in HicoDetInstanceSplit._splits
+
+    for split, hds in [(Splits.TRAIN, train_split),
+                       (Splits.TEST, test_split),
+                       ]:
+        hd_loader = hds.get_loader(batch_size=batch_size, shuffle=False, drop_last=False)
+
+        precomputed_feats_fn = cfg.program.precomputed_feats_file_format % (cfg.model.rcnn_arch, split.value)
+        feat_file = h5py.File(precomputed_feats_fn, 'r+')
+
+        try:
+            all_img_infos = []
+            for im_i, im_data in enumerate(hd_loader):
+                im_data = im_data  # type: Minibatch
+                assert len(im_data.other_ex_data) == 1
+                assert im_data.other_ex_data[0]['flipped'] == flipping
+
+                all_img_infos.append(im_data.img_infos.cpu().numpy())
+
+            assert len(all_img_infos) == len(hd_loader)
+            all_img_infos = np.concatenate([x for x in all_img_infos if x is not None], axis=0)
+            assert all_img_infos.shape[0] == len(hd_loader)
+            feat_file.create_dataset('img_infos', data=all_img_infos.astype(np.int))
+
+        finally:
+            feat_file.close()
+            print('%s feat file closed.' % split.value)
+
+
+if __name__ == '__main__':
+    save_feats()
