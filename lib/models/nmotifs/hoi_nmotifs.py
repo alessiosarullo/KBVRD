a3d8d4692fac7f4c80d9173a37738b3de0aa1b3d
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-03-20 17:48:33 +0000

    model: unbiased nmotifs

--- lib/models/nmotifs/hoi_nmotifs.py
+++ lib/models/nmotifs/hoi_nmotifs.py
@@ -87,7 +87,7 @@ class NMotifsHOIBranch(AbstractHOIBranch):
     def output_dim(self):
         raise NotImplementedError()
 
-    def _forward(self, boxes_ext, box_feats, hoi_infos, union_box_feats, box_labels):
+    def _forward(self, boxes_ext, box_feats, hoi_infos, union_box_feats, box_labels, use_bias=True):
         obj_logits, obj_classes, edge_ctx = self.context(box_feats, boxes_ext, box_labels)
 
         edge_repr = self.post_lstm(edge_ctx)
@@ -101,74 +101,17 @@ class NMotifsHOIBranch(AbstractHOIBranch):
         hoi_repr = sub_repr * obj_repr * union_box_feats
 
         hoi_logits = self.hoi_output_fc(hoi_repr)
-        hoi_logits = hoi_logits + self.freq_bias.index_with_labels(obj_classes[obj_inds])
+        if use_bias:
+            hoi_logits = hoi_logits + self.freq_bias.index_with_labels(obj_classes[obj_inds])
 
         return obj_logits, hoi_logits
 
 
-class NMotifsKB(GenericModel):
+class NMotifsKB(NMotifs):
     @classmethod
     def get_cline_name(cls):
-        return 'nmotifs-kb'
-
-    def __init__(self, dataset: HicoDetInstanceSplit, **kwargs):
-        super().__init__(dataset, **kwargs)
-        self.hoi_branch = NMotifsKBHOIBranch(self.dataset, self.visual_module.vis_feat_dim)
+        return 'nmotifs-nobias'
 
     def _forward(self, boxes_ext, box_feats, masks, union_boxes_feats, hoi_infos, box_labels=None, hoi_labels=None):
-        obj_logits, hoi_logits = self.hoi_branch(boxes_ext, box_feats, hoi_infos, union_boxes_feats, box_labels)
-        return obj_logits, hoi_logits
-
-
-class NMotifsKBHOIBranch(AbstractHOIBranch):
-    def __init__(self, dataset: HicoDetInstanceSplit, visual_feats_dim):
-        super().__init__()
-
-        self.context = LinearizedContext(classes=dataset.objects, visual_feat_dim=visual_feats_dim)
-
-        # Initialize to sqrt(1/2n) so that the outputs all have mean 0 and variance 1 (half contribution comes from LSTM, half from embedding).
-        # In practice the pre-lstm stuff tends to have stdev 0.1 so I multiplied this by 10.
-        self.post_lstm = nn.Linear(self.context.edge_ctx_dim, visual_feats_dim * 2)
-        torch.nn.init.normal_(self.post_lstm.weight, mean=0, std=10 * math.sqrt(1.0 / self.context.edge_ctx_dim))
-        torch.nn.init.zeros_(self.post_lstm.bias)
-
-        self.hoi_output_fc = nn.Linear(visual_feats_dim, dataset.num_predicates, bias=True)
-        torch.nn.init.xavier_normal_(self.hoi_output_fc.weight, gain=1.0)
-
-        self.freq_bias = FrequencyBias(dataset=dataset)
-
-        # # # KB
-        eps = 1e-3
-        imsitu_prior_matrix = ImSituKnowledgeExtractor().extract_prior_matrix(dataset).T  # P x O
-        # We don't want to penalise predicates that are not present
-        imsitu_prior_matrix[~np.any(imsitu_prior_matrix, axis=1), :] = 1
-        pred_obj_prior = np.log(imsitu_prior_matrix / np.sum(imsitu_prior_matrix, axis=1, keepdims=True) + 1e-3)
-        assert not (np.any(np.isnan(pred_obj_prior)) or np.any(np.isinf(pred_obj_prior)))
-        obj_pred_prior = torch.from_numpy(pred_obj_prior.T).float()
-        assert obj_pred_prior.shape == (dataset.num_object_classes, dataset.num_predicates)
-        self.imsitu_prior = nn.Embedding.from_pretrained(obj_pred_prior, freeze=True)
-
-    @property
-    def output_dim(self):
-        raise NotImplementedError()
-
-    def _forward(self, boxes_ext, box_feats, hoi_infos, union_box_feats, box_labels):
-        obj_logits, obj_classes, edge_ctx = self.context(box_feats, boxes_ext, box_labels)
-
-        edge_repr = self.post_lstm(edge_ctx)
-        edge_repr = edge_repr.view(edge_repr.shape[0], 2, -1)  # Split into subject and object representations
-
-        sub_inds = hoi_infos[:, 1]
-        obj_inds = hoi_infos[:, 2]
-
-        sub_repr = edge_repr[:, 0][sub_inds]
-        obj_repr = edge_repr[:, 1][obj_inds]
-        hoi_repr = sub_repr * obj_repr * union_box_feats
-
-        hoi_logits = self.hoi_output_fc(hoi_repr)
-        hoi_logits = hoi_logits + self.freq_bias.index_with_labels(obj_classes[obj_inds])
-
-        imsitu_hoi_logits = self.imsitu_prior(obj_classes[obj_inds])
-        hoi_logits += imsitu_hoi_logits
-
+        obj_logits, hoi_logits = self.hoi_branch(boxes_ext, box_feats, hoi_infos, union_boxes_feats, box_labels, use_bias=False)
         return obj_logits, hoi_logits
