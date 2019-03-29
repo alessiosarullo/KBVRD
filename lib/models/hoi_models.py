21952ae8aab33983794721d260533741422fed63
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-03-29 18:04:01 +0000

    bugfix

--- lib/models/hoi_models.py
+++ lib/models/hoi_models.py
@@ -115,9 +115,9 @@ class NMotifsHOIBranch(AbstractHOIBranch):
 class MemNMotifsHOIBranch(NMotifsHOIBranch):
     def __init__(self, visual_feats_dim, obj_feat_dim, dataset, **kwargs):
         self.mem_att_entropy = 1 / 0.1
-        self.memory_output_size = 1024
         super().__init__(visual_feats_dim, obj_feat_dim, dataset, **kwargs)
         self.memory_input_size = self.hoi_repr_dim
+        self.memory_output_size = self.hoi_repr_dim
         memory_size = dataset.num_predicates  # this CANNOT be modified without changing the forward pass
 
         mem = torch.empty(memory_size, self.memory_input_size)
