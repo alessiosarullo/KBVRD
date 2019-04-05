e72c6e4c8503b75dba7f140d2798e1eb5dc96b7e
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-04-05 12:10:40 +0100

    bugfix

--- lib/models/hoi_branches.py
+++ lib/models/hoi_branches.py
@@ -233,11 +233,13 @@ class MemHoiBranch(AbstractHOIBranch):
 
         if hoi_labels is not None:
             mem_pred = hoi_labels @ self.memory_values
-            margin = (1 - hoi_labels) @ mem_sim - hoi_labels @ mem_sim
+            
+            mem_sim_t = (input_mem_repr[:, None, :] * self.memory_keys[None, :, :]).sum(dim=2)  # H x M
+            margin_loss = ((1 - hoi_labels) * mem_sim_t - hoi_labels * mem_sim_t).sum(dim=1)
         else:
-            mem_pred = margin = None
+            mem_pred = margin_loss = None
 
-        return hoi_repr, mem_pred, margin
+        return hoi_repr, mem_pred, margin_loss
 
 
 class HoiPriorBranch(AbstractHOIBranch):
