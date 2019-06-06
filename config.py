0cd88478f914ab57d0486f6d215d1563d7d6e156
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-06-06 10:26:54 +0100

    bugfix

--- config.py
+++ config.py
@@ -205,7 +205,7 @@ class OptimizerConfig(BaseConfigs):
 
         # Learning rate parameters. Use gamma > 0 to enable decay at the specified interval
         self.lr = 1e-3
-        self.lr_gamma = 0
+        self.lr_gamma = 0.0
         self.lr_decay_period = 2
 
         self.num_epochs = 10
