f95e145ef0774d6bd8bdef8ccbf4852890fa9114
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-06-28 15:09:08 +0100

    bugfix

--- lib/models/hoi_branches.py
+++ lib/models/hoi_branches.py
@@ -17,7 +17,7 @@ class CheatGCNBranch(AbstractHOIBranch):
         self.num_predicates = dataset.hicodet.num_predicates
 
         # Normalised adjacency matrix
-        self.noun_verb_links = torch.from_numpy((dataset.hicodet.op_pair_to_interaction >= 0).astype(np.float32))
+        self.noun_verb_links = nn.Parameter(torch.from_numpy((dataset.hicodet.op_pair_to_interaction >= 0).astype(np.float32)), requires_grad=False)
         adj = torch.eye(self.num_objects + self.num_predicates).float()
         adj[:self.num_objects, self.num_objects:] = self.noun_verb_links  # top right
         adj[self.num_objects:, :self.num_objects] = self.noun_verb_links.t()  # bottom left
