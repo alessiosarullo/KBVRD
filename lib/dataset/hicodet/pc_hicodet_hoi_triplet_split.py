c3679d41bffba9457f8c80e60c413aa4d5a7cf02
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-07-01 14:10:15 +0100

    bugfix

--- lib/dataset/hicodet/pc_hicodet_hoi_triplet_split.py
+++ lib/dataset/hicodet/pc_hicodet_hoi_triplet_split.py
@@ -19,9 +19,10 @@ class PrecomputedHicoDetPureHOISplit(PrecomputedHicoDetSplit):
         self.pc_im_idx_to_im_idx = {}
         for pc_im_idx, pc_im_id in enumerate(self.pc_image_ids):
             im_idx = np.flatnonzero(self.image_ids == pc_im_id).tolist()  # type: List
-            assert len(im_idx) == 1, im_idx
-            assert pc_im_id not in self.pc_im_idx_to_im_idx
-            self.pc_im_idx_to_im_idx[pc_im_idx] = im_idx[0]
+            if im_idx:
+                assert len(im_idx) == 1, im_idx
+                assert pc_im_id not in self.pc_im_idx_to_im_idx
+                self.pc_im_idx_to_im_idx[pc_im_idx] = im_idx[0]
 
     def get_loader(self, batch_size, num_workers=0, num_gpus=1, shuffle=None, drop_last=True, **kwargs):
         if shuffle is None:
