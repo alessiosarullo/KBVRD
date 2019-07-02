8d1698c905f59bbadca88aa9b9a5fbe51a8005e1
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-07-02 15:00:45 +0100

    bugfix

--- lib/dataset/hicodet/pc_hicodet_singlehois_split.py
+++ lib/dataset/hicodet/pc_hicodet_singlehois_split.py
@@ -78,6 +78,7 @@ class PrecomputedHicoDetSingleHOIsSplit(PrecomputedHicoDetSplit):
         hoi_infos[1:] = [0, 1]
         entry.precomp_hoi_infos = hoi_infos[None, :]
         entry.precomp_hoi_union_boxes = self.pc_union_boxes[[pc_hoi_idx], :]
+        entry.precomp_hoi_union_feats = self.pc_union_boxes_feats[[pc_hoi_idx], :]
         entry.precomp_action_labels = self.pc_action_labels[[pc_hoi_idx], :].copy()
 
         Timer.get('GetBatch').toc()
