92ec254bf7e3c3d7ce68f82fed66aa1392ae08cc
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-07-24 19:44:45 +0100

    bugfix

--- lib/dataset/hico/hico_split.py
+++ lib/dataset/hico/hico_split.py
@@ -33,7 +33,7 @@ class HicoSplit(Dataset):
         self.objects = [hico.objects[i] for i in object_inds]
         self.active_object_classes = np.array(object_inds, dtype=np.int)
 
-        predicate_inds = sorted(predicate_inds) if object_inds is not None else range(self.hico.num_predicates)
+        predicate_inds = sorted(predicate_inds) if predicate_inds is not None else range(self.hico.num_predicates)
         self.predicates = [hico.predicates[i] for i in predicate_inds]
         self.active_predicates = np.array(predicate_inds, dtype=np.int)
 
