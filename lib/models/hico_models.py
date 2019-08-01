bc9e092be5564952442d3178f684285f2c28022d
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-08-01 16:40:43 +0100

    hicocomp

--- lib/models/hico_models.py
+++ lib/models/hico_models.py
@@ -738,6 +738,74 @@ class HicoM(HicoExtZSGCMultiModel):
         return obj_logits, act_logits, hoi_logits, obj_labels, act_labels, hoi_labels, reg_loss
 
 
+class HicoComp(HicoExtZSGCMultiModel):
+    @classmethod
+    def get_cline_name(cls):
+        return 'hicocomp'
+
+    def forward(self, x: List[torch.Tensor], inference=True, **kwargs):
+        with torch.set_grad_enabled(self.training):
+
+            feats, labels = x
+            obj_logits, act_logits, hoi_logits, obj_labels, act_labels, hoi_labels, reg_loss = self._forward(feats, labels)
+
+            if not inference:
+                obj_labels.clamp_(min=0), act_labels.clamp_(min=0), hoi_labels.clamp_(min=0)
+                if self.zs_enabled:
+                    if cfg.softl > 0:
+                        if cfg.hico_zsa:
+                            losses = {'act_loss': bce_loss(act_logits[:, self.seen_act_inds], act_labels[:, self.seen_act_inds]),
+                                      'act_loss_unseen': cfg.softl * bce_loss(act_logits[:, self.unseen_act_inds],
+                                                                              act_labels[:, self.unseen_act_inds]),
+                                      'hoi_loss': bce_loss(hoi_logits, hoi_labels),
+                                      }
+                        else:
+                            losses = {'act_loss': bce_loss(act_logits, act_labels),
+                                      'hoi_loss': bce_loss(hoi_logits[:, self.seen_hoi_inds], hoi_labels[:, self.seen_hoi_inds]),
+                                      'hoi_loss_unseen': cfg.softl * bce_loss(hoi_logits[:, self.unseen_hoi_inds],
+                                                                              hoi_labels[:, self.unseen_hoi_inds])
+                                      }
+                        losses['obj_loss'] = bce_loss(obj_logits, obj_labels)
+                    else:
+                        losses = {'obj_loss': cfg.olc * bce_loss(obj_logits[:, self.seen_obj_inds], obj_labels[:, self.seen_obj_inds]),
+                                  'act_loss': cfg.alc * bce_loss(act_logits[:, self.seen_act_inds], act_labels[:, self.seen_act_inds]),
+                                  'hoi_loss': cfg.hlc * bce_loss(hoi_logits[:, self.seen_hoi_inds], hoi_labels[:, self.seen_hoi_inds]),
+                                  }
+                else:
+                    losses = {'obj_loss': bce_loss(obj_logits, obj_labels),
+                              'act_loss': bce_loss(act_logits, act_labels),
+                              'hoi_loss': bce_loss(hoi_logits, hoi_labels),
+                              }
+                if reg_loss is not None:
+                    losses['reg_loss'] = reg_loss
+                return losses
+            else:
+                prediction = Prediction()
+                interactions = self.dataset.full_dataset.interactions
+                obj_scores = torch.sigmoid(obj_logits).cpu().numpy()
+                act_scores = torch.sigmoid(act_logits).cpu().numpy()
+                prediction.hoi_scores = obj_scores[:, interactions[:, 1]] * act_scores[:, interactions[:, 0]]
+                return prediction
+
+    def _forward(self, feats, labels):
+        obj_logits, act_logits, hoi_logits = self.multi_model._forward(feats, labels)
+
+        if labels is not None:
+            hoi_labels = labels.clamp(min=0)
+            obj_labels = interactions_to_objects(hoi_labels, self.dataset.full_dataset).detach()
+            act_labels = interactions_to_actions(hoi_labels, self.dataset.full_dataset).detach()
+            if cfg.softl > 0:
+                if cfg.hico_zsa:
+                    act_labels[:, self.unseen_act_inds] = self.get_soft_labels(labels)
+                else:
+                    hoi_labels[:, self.unseen_hoi_inds] = self.get_soft_labels(labels)
+        else:
+            obj_labels = act_labels = hoi_labels = None
+
+        reg_loss = None
+        return obj_logits, act_logits, hoi_logits, obj_labels, act_labels, hoi_labels, reg_loss
+
+
 class KatoModel(HicoExtKnowledgeGenericModel):
     @classmethod
     def get_cline_name(cls):
