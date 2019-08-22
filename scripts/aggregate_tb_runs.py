2c27ab83220ff3a5ff1fa3d790a401d9389636f0
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-08-22 18:46:08 +0100

    multiple runs script

--- scripts/aggregate_tb_runs.py
+++ scripts/aggregate_tb_runs.py
@@ -1,26 +1,45 @@
-import os
 import argparse
+import os
 
+import numpy as np
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+from tensorboardX import SummaryWriter
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('fname')
     parser.add_argument('runs', nargs=argparse.REMAINDER)
     args = parser.parse_args()
+    fname = args.fname
     runs = args.runs
 
+    print(fname)
     print(runs)
 
     summary_iterators = [EventAccumulator(os.path.join(p, 'tboard/Test')).Reload() for p in runs]
     tags = summary_iterators[0].Tags()['scalars']
     for it in summary_iterators:
         assert it.Tags()['scalars'] == tags
-    out = {}
+
+    values_per_tag = {}
+    steps = []
     for tag in tags:
         for events in zip(*[acc.Scalars(tag) for acc in summary_iterators]):
-            assert len(set(e.step for e in events)) == 1
-            out.setdefault(tag, []).append([e.value for e in events])
+            values_per_tag.setdefault(tag, []).append([e.value for e in events])
+            assert len({e.step for e in events}) == 1
+            if len(values_per_tag.keys()) == 1:
+                steps.append(events[0].step)
+    steps = np.array(steps)
+
+    aggr_ops = {'mean': np.mean,
+                'std': np.std}
+    for aggr_op_name, aggr_op in aggr_ops.items():
+        tblogger = SummaryWriter(os.path.join(fname, 'tboard/Test', aggr_op_name))
+        for tag, values in values_per_tag.items():
+            aggr_value_per_timestep = aggr_op(np.array(values), axis=1)
+            for i, aggr_value in enumerate(aggr_value_per_timestep):
+                tblogger.add_scalar(tag, aggr_value, global_step=steps[i])
 
 
 if __name__ == '__main__':
--- scripts/multiple_runs.sh
+++ scripts/multiple_runs.sh
@@ -14,17 +14,18 @@ export CUDA_VISIBLE_DEVICES=$GPU_ID
 OUTPUT_DIR="output/${NET}"
 
 EXPS=()
-for IDX in `seq 1 ${NUM_RUNS}`
+for IDX in $(seq 1 "${NUM_RUNS}")
 do
-  DATETIME=`date +'%Y-%m-%d_%H-%M-%S'`
+  DATETIME=$(date +'%Y-%m-%d_%H-%M-%S')
   EXP_FULL_NAME="${DATETIME}_${EXP_NAME}_RUN${IDX}"
   EXP_DIR=${OUTPUT_DIR}/${EXP_FULL_NAME}
   LOG="$EXP_DIR/log.txt"
 
   EXPS+=("${EXP_DIR}")
-#  mkdir -p ${EXP_DIR}
-#  echo Logging ${EXP_DIR} to "$LOG"
-#
-#  python -u scripts/run.py --model ${NET} --save_dir ${EXP_FULL_NAME} --randomize "${@:5}" > ${LOG} 2>&1
+  mkdir -p "${EXP_DIR}"
+  echo Logging "${EXP_DIR}" to "$LOG"
+
+  python -u scripts/run.py --model "${NET}" --save_dir "${EXP_FULL_NAME}" --randomize "${@:5}" > "${LOG}" 2>&1
 done
-python -u scripts/aggregate_tb_runs.py "${EXPS[@]}"
+DATETIME=$(date +'%Y-%m-%d_%H-%M-%S')
+python -u scripts/aggregate_tb_runs.py "${DATETIME}_${EXP_NAME}_AGGR${NUM_RUNS}" "${EXPS[@]}"
