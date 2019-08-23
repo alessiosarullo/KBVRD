9ac00758288972a8938c304879bc162235eaaad0
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-08-23 16:38:09 +0100

    defaults updated

--- config.py
+++ config.py
@@ -111,7 +111,7 @@ class Configs:
         self.greg_margin = 0.3
         self.vv = False
         self.aggp = False
-        self.gcldim = 200
+        self.gcldim = 512
         self.gcrdim = 1024
         self.gcdetach = False
 
