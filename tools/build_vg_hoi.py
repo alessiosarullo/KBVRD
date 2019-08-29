a5e9581309d68f82eaaa415c9095558f47726227
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-08-29 11:03:55 +0100

    VG HOI script

---
+++ tools/build_vg_hoi.py
@@ -0,0 +1,32 @@
+import json
+import pickle
+
+
+def main():
+    relationships_data = json.load(open('relationships.json', 'r'))
+
+    image_ids = [img_rdata['image_id'] for img_rdata in relationships_data]
+    relationships = []
+    for img_rdata in relationships_data:
+        img_rs = []
+        for rdict in img_rdata['relationships']:
+            try:
+                subj = rdict['subject']['name']
+            except KeyError:
+                subj = rdict['subject']['names'][0]
+            pred = rdict['predicate']
+            try:
+                obj = rdict['object']['name']
+            except KeyError:
+                obj = rdict['object']['names'][0]
+            img_rs.append([subj, pred, obj])
+        relationships.append(img_rs)
+
+    with open('vg_hoi.pkl') as f:
+        pickle.dump({'image_ids': image_ids,
+                     'relationships': relationships},
+                    f)
+
+
+if __name__ == '__main__':
+    main()
