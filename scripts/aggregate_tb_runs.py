9aa22e5414ba8c9b667a319e3181b17d337f9b36
Alessio (Susy) <alessio.sarullo@gmail.com>
2019-10-10 12:11:34 +0100

    sync

--- scripts/aggregate_tb_runs.py
+++ scripts/aggregate_tb_runs.py
@@ -29,17 +29,17 @@ def main():
     os.makedirs(fname, exist_ok=True)
 
     runs_data = get_runs_data(runs)
-    values_per_tag = runs_data['Test']['values']
-    steps = runs_data['Test']['steps']
+    for split in ['Train', 'Val', 'Test']:
+        values_per_tag = runs_data[split]['values']
+        steps = runs_data[split]['steps']
 
-    aggr_ops = {'mean': np.mean,
-                'std': np.std}
-    for aggr_op_name, aggr_op in aggr_ops.items():
-        tblogger = SummaryWriter(os.path.join(fname, 'tboard/Test', aggr_op_name))
-        for tag, values in values_per_tag.items():
-            aggr_value_per_timestep = aggr_op(values, axis=0)
-            for i, aggr_value in enumerate(aggr_value_per_timestep):
-                tblogger.add_scalar(tag, aggr_value, global_step=steps[i])
+        aggr_ops = {'mean': np.mean}
+        for aggr_op_name, aggr_op in aggr_ops.items():
+            tblogger = SummaryWriter(os.path.join(fname, 'tboard', split, aggr_op_name))
+            for tag, values in values_per_tag.items():
+                aggr_value_per_timestep = aggr_op(values, axis=0)
+                for i, aggr_value in enumerate(aggr_value_per_timestep):
+                    tblogger.add_scalar(tag, aggr_value, global_step=steps[i])
 
 
 if __name__ == '__main__':
